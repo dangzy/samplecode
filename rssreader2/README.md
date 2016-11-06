8b49c194b496381f1eb2c46377560278a396f592
dangzy <dangzy@yeah.net>
2016-11-06 15:00:48 +0800

    add rssreader2

---
+++ rssreader2/README.md
@@ -0,0 +1,19 @@
+#Dependence
+Depend on:
+
+* [tinyxml2](https://github.com/leethomason/tinyxml2)
+* [curl](https://github.com/curl/curl)
+
+#Compile
+<code>
+g++ rss.cpp -ltinyxml2 -s -o rss
+</code>
+
+#Usage
+<code>
+./rss
+</code>
+or
+<code>
+./rss rss.xml
+</code>
---
+++ rssreader2/rss.cpp
@@ -0,0 +1,106 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <tinyxml2.h>
+#include <unistd.h>
+#include <cstring>
+
+using namespace std;
+namespace xml=tinyxml2;
+
+static vector<string> getxmls(const char* cfg) {
+    vector<string> xmls;
+    xml::XMLDocument doc;
+    doc.LoadFile(cfg);
+    xml::XMLElement *e = doc.FirstChildElement("rss");
+    while(e) {
+        xmls.push_back(e->GetText());
+        e = e->NextSiblingElement();
+    }
+    return xmls;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc > 2){
+        cerr << "Param err" << endl;
+        return -1;
+    }
+    vector<string> xmls = getxmls(argc==1?"rss.xml":argv[1]);
+    if (0 == xmls.size()) {
+        cerr << "No xml" << endl;
+        return -2;
+    }
+    bool showdetail = false;
+    bool iswin = strlen(getenv("WINDIR")) > 0?true:false;
+    char url[1024] = {0};
+    const char* tmpfile="tmp.xml";
+    const char* mdfile="rss.html";
+    int rc = -1;
+    string htmlfile = (iswin?getenv("TMP"):getenv("HOME"))+string("/")+mdfile;
+    ofstream md(htmlfile.c_str());
+    md<< "<html><head><meta charset=\"utf-8\"></head><body>" << endl;
+    int cnt = 0;
+    for(int i=0;i < xmls.size();i++) {
+        sprintf(url,"curl -s -o %s %s",tmpfile,xmls[i].c_str());
+        rc = system(url);
+        if (rc == 0) {
+            xml::XMLDocument doc;
+            doc.LoadFile(tmpfile);
+            xml::XMLElement *e = doc.FirstChildElement("rss");
+            if (e) {
+                e = e->FirstChildElement("channel");
+                xml::XMLElement *channel = e->FirstChildElement("title");
+                md << "<h1>" << channel->GetText() << "</h1>" <<endl;
+                e = e->FirstChildElement("item");
+                while (e) {
+                    xml::XMLElement *item = e->FirstChildElement("title");
+                    if (item){
+                        cnt++;
+                        md << "<a href=\"" << e->FirstChildElement("link")->GetText() << "\" target=\"_blank\">"<< item->GetText() << "</a><br>" << endl;
+                    }
+                    if(showdetail) {
+                        item = e->FirstChildElement("description");
+                        if (item){
+                            md << "<div>" << item->GetText() << "</div><br>" << endl;
+                        }
+                    } else
+                        md << "<br>" << endl;
+                    e = e->NextSiblingElement();
+                }
+            } else {
+                e = doc.FirstChildElement("feed");
+                if (e) {
+                    e = e->FirstChildElement("title");
+                    md << "<h1>" << e->GetText() << "</h1>" <<endl;
+                    e = doc.FirstChildElement("feed")->FirstChildElement("entry");
+                    while(e) {
+                        xml::XMLElement *item = e->FirstChildElement("title");
+                        if (item){
+                            cnt++;
+                            md << "<a href=\"" << e->FirstChildElement("id")->GetText() << "\" target=\"_blank\">"<< item->GetText() << "</a><br>"<< endl;
+                        }
+                        if(showdetail){
+                            item = e->FirstChildElement("summary");
+                            if (item){
+                                md << "<div>" << item->GetText() << "</div><br>" << endl;
+                            }
+                        } else
+                            md << "<br>" << endl;
+                        e = e->NextSiblingElement();
+                    }
+                } else
+                    cerr << "don't support " << xmls[i] << endl;
+            }
+            unlink(tmpfile);
+        } else
+            cerr << "curl err: " << url << endl;
+    }
+    md << "</body></html>";
+    md.close();
+    if (iswin)
+        sprintf(url,"start %s",htmlfile.c_str());
+    else
+        sprintf(url,"x-www-browser %s",htmlfile.c_str());
+    system(url);
+    return 0;
+}
